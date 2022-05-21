b5b833c8fd6d4ae172d39fb3c09611217516bc13
Rafael Pinheiro <rafael.c.pinheiro@gmail.com>
2022-05-21 20:13:26 -0300

    Removed the C++ dependencies

--- Source/TopDown/TopDown.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TopDown.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TopDown, "TopDown" );
-
-DEFINE_LOG_CATEGORY(LogTopDown)
- 
